287a60e9ddddea3a85632d72d8979699d16d8a99
lyapi <daniilyapin444@gmail.com>
2025-06-21 22:57:39 +0400

    Удалил ветку main

--- app/api/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.routes.UserRout import router as user_rout
-
-api_router = APIRouter()
-
-api_router.include_router(user_rout, prefix="/user", tags=["user"])
--- app/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.models.dbModels.User import UserEntity
