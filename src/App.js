1ad04e50d8f2c9e39652080397b31eb10ed3d791
lyapi <daniilyapin444@gmail.com>
2025-07-20 20:55:47 +0400

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button as SDSButton } from './sds/src/ui/primitives/Button/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SDSButton variant="primary">Тест SDS</SDSButton>
     </div>
   );
 }
